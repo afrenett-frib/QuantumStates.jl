17cb8701be304fbb7cd8d3bf24c6ab87725a0ee8
afrenett-frib <frenett@frib.msu.edu>
2026-02-23 18:31:15 +0100

    Update HundsCaseA_LinearMolecule.jl

--- src/HundsCaseA_LinearMolecule.jl
+++ src/HundsCaseA_LinearMolecule.jl
@@ -267,11 +267,11 @@ function Hyperfine_IJ_od(state::HundsCaseA_LinearMolecule, state′::HundsCaseA_
     #from doi.org/10.1063/1.1712160
     v_1,  v_2,  ℓ,  v_3,  Λ,  K,  I,  S,  Σ,  J,  P,  F,  M  = unpack(state)
     v_1′, v_2′, ℓ′, v_3′, Λ′, K′, I′, S′, Σ′, J′, P′, F′, M′ = unpack(state′)
-    if ~delta(state, state′, :F, :M, :P)
+    if ~delta(state, state′, :Σ, :Λ, :K, :Σ, :P, :F, :M)
         return 0.0
     else
         return (
-             δ(I, I')*δ(J, J′-1)*(-1)*sqrt(J^2 - P^2) * (2*J*sqrt(4J^2-1))^(-1)*sqrt((F-I+J)*(F+J+I+1)*(J+I-F)*(F-J+I+1))
+              wigner3j(J, 1, J′, -P, 0, P′)*(-1)*sqrt(J^2 - P^2) * (2*J*sqrt(4J^2-1))^(-1)*sqrt((F-I+J)*(F+J+I+1)*(J+I-F)*(F-J+I+1))
         )
     end
 end
