b039537ee650528ab4222838b81b761604950a42
afrenett-frib <frenett@frib.msu.edu>
2026-02-23 18:10:34 +0100

    Update HundsCaseA_LinearMolecule.jl

--- src/HundsCaseA_LinearMolecule.jl
+++ src/HundsCaseA_LinearMolecule.jl
@@ -271,7 +271,7 @@ function Hyperfine_IJ_od(state::HundsCaseA_LinearMolecule, state′::HundsCaseA_
         return 0.0
     else
         return (
-             δ(J, J′-1)*(-1)*sqrt(J^2 - P^2) * (2*J*sqrt(4J^2-1))^(-1)*sqrt((F-I+J)*(F+J+I+1)*(J+I-F)*(F-J+I+1))
+             δ(J, J′+1)*(-1)*sqrt(J^2 - P^2) * (2*J*sqrt(4J^2-1))^(-1)*sqrt((F-I+J)*(F+J+I+1)*(J+I-F)*(F-J+I+1))
         )
     end
 end
